5696ad0ba4d7131fd3ba922076e45f957510a3e2
ubbba <88180747+ubbba@users.noreply.github.com>
2023-06-23 14:32:19 +0900

    6/23 first

---
+++ js/start.js
@@ -0,0 +1,9 @@
+"use scrict";
+
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+
+function begin() {
+  main.style.display = "none";
+  qna.style.display = "block";
+}
