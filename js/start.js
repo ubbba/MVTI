494c66c92995d0a4ffbe1de8f882554b24b03ad4
ubbba <88180747+ubbba@users.noreply.github.com>
2023-06-23 16:59:31 +0900

    6/23 third

--- js/start.js
+++ js/start.js
@@ -1,11 +1,54 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
 
-function addAnswer(answerText, qNum) {
-  var a = document.querySelector(".answer");
+const endPoint = 12;
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+function calResult() {
+  console.log(select);
+  var result = select.indexOf(Math.max(...select));
+  return result;
+}
+
+function setResult() {
+  let point = calResult();
+  const resultName = document.querySelector(".resultname");
+  resultName.innerHTML = infoList[point].name;
+
+  var resultImg = document.createElement("img");
+  const imgDiv = document.querySelector("#resultImg");
+  var imgURL = "img/image-" + point + ".png";
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add("img-fluid");
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector(".resultDesc");
+  resultDesc.innerHTML = infoList[point].desc;
+}
+
+function goResult() {
+  qna.style.WebkitAnimation = "fadeOut 1s";
+  qna.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    result.style.WebkitAnimation = "fadeIn 1s";
+    result.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      qna.style.display = "none";
+      result.style.display = "block";
+    }, 450);
+  });
+  setResult();
+}
+
+function addAnswer(answerText, qIdx, idx) {
+  var a = document.querySelector(".answerBox");
   var answer = document.createElement("button");
   answer.classList.add("answerList");
-  // a안에서 answer가 소속될 수 있게
+
+  answer.classList.add("fadeIn");
+
   a.appendChild(answer);
   answer.innerHTML = answerText;
 
@@ -15,35 +58,51 @@ function addAnswer(answerText, qNum) {
       var children = document.querySelectorAll(".answerList");
       for (let i = 0; i < children.length; i++) {
         children[i].disabled = true;
-        children[i].style.display = "none";
+        children[i].style.WebkitAnimation = "fadeOut 0.5s";
+        children[i].style.animation = "fadeOut 0.5s";
       }
-      goNext(++qNum);
+      setTimeout(() => {
+        var target = qnaList[qIdx].a[idx].type;
+        for (let i = 0; i < target.length; i++) {
+          select[target[i]] += 1;
+        }
+
+        for (let i = 0; i < children.length; i++) {
+          children[i].style.display = "none";
+        }
+        goNext(++qIdx);
+      }, 450);
     },
     false
   );
 }
 
-function goNext(qNum) {
-  var q = document.querySelector(".question");
-  q.innerHTML = qnaList[qNum].q;
-  for (let i in qnaList[qNum].a) {
-    addAnswer(qnaList[qNum].a[i].answer, qNum);
+function goNext(qIdx) {
+  if (qIdx === endPoint) {
+    goResult();
+    return;
   }
+
+  var q = document.querySelector(".qBox");
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+  }
+  var status = document.querySelector(".statusBar");
+  status.style.width = (100 / endPoint) * (qIdx + 1) + "%";
 }
 
 function begin() {
-  main.style.animation = "fadeOut 1s ";
-  main.style.WebkitAnimation = "fadeOut 1s ";
-
+  main.style.WebkitAnimation = "fadeOut 1s";
+  main.style.animation = "fadeOut 1s";
   setTimeout(() => {
-    qna.style.animation = "fadeIn 1s ";
-    qna.style.WebkitAnimation = "fadeIn 1s ";
-
+    qna.style.WebkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
-      qna.style.display = "block";
       main.style.display = "none";
-    }, 400);
-    let qNum = 0;
-    goNext(qNum);
-  }, 300);
+      qna.style.display = "block";
+    }, 450);
+    let qIdx = 0;
+    goNext(qIdx);
+  }, 450);
 }
